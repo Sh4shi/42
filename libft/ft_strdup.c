114db8af10a6cbdf1a7e8d8df75866fde3480851
Sharon <sharondellunto@icloud.com>
2022-01-11 19:34:42 +0100

    libft

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -3,16 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sdell-un <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sdell-un <sdell-un@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 18:12:21 by sdell-un          #+#    #+#             */
-/*   Updated: 2022/01/11 18:25:34 by sdell-un         ###   ########.fr       */
+/*   Updated: 2022/01/11 19:34:37 by sdell-un         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *strdup(const char *s1)
+char	*strdup(const char *s1)
 {
+	char *s2;
+	int i;
 	int len;
-	char s2[];
-
+	
 	len = ft_strlen(s1);
+	s2 = (char *)malloc(sizeof (*s2) * len);
+	i = 0;
+	while (i < len)
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	return (*s2);
+}
