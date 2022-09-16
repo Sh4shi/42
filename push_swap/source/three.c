01d2692d3cd893141c6c1736be5268ea93b0a867
Sh4shi <sharondellunto@icloud.com>
2022-09-16 23:27:18 +0200

    fix three sort

--- push_swap/source/three.c
+++ push_swap/source/three.c
@@ -4,16 +4,49 @@
 
 #include "../include/push_swap.h"
 
+int     find_list_max(t_node *node)
+{
+    int max;
+
+    max = node->data;
+    while (node)
+    {
+        if (node->data > max)
+            max = node->data;
+        node = node->next;
+    }
+    return (max);
+}
+
+int     find_list_min(t_node *node)
+{
+    int min;
+
+    min = node->data;
+    while (node)
+    {
+        if (node->data < min)
+            min = node->data;
+        node = node->next;
+    }
+    return (min);
+}
+
 void    three_sort(t_stack *stack)
 {
-    if (stack->head == stack->min)
+    int max;
+    int min;
+
+    max = find_list_max(stack->head);
+    min = find_list_min(stack->head);
+    if (stack->head->data == min)
     {
         rra_rrb(stack, 'a', 1);
         sa_sb(stack, 'a', 1);
     }
-    else if (stack->tail == stack->min)
+    else if (stack->tail->data == min)
     {
-        if(stack->head == stack->max)
+        if(stack->head->data == max)
         {
             sa_sb(stack, 'a', 1);
             rra_rrb(stack, 'a', 1);
@@ -21,7 +54,7 @@ void    three_sort(t_stack *stack)
         else
             rra_rrb(stack, 'a', 1);
     }
-    else if (stack->head == stack->max)
+    else if (stack->head->data == max)
         ra_rb(stack, 'a', 1);
     else
         sa_sb(stack, 'a', 1);
