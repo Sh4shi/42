f4effaeaa455470207fcc1f7ad91f07f95591399
sh4shi <sharondellunto@icloud.com>
2022-11-13 17:52:38 +0100

    makefile compila minilib

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -1,1 +1,19 @@
-#include "./../include/so_long.h"
+//#include "./../include/so_long.h"
+
+#include <stdio.h>
+#include <mlx.h>
+
+int main(void)
+{
+    void *mlx_ptr = mlx_init();
+
+    if (!mlx_ptr)
+        return puts("mlx_ptr");
+
+    void *win_ptr = mlx_new_window(mlx_ptr, 200, 200, "Sharon");
+
+    if (!win_ptr)
+        return puts("win_ptr");
+
+    mlx_loop(mlx_ptr);
+}
