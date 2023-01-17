5b0bc41f98ecc31ccac3ea9037bfac3ab70719e6
sh4shi <sharondellunto@icloud.com>
2023-01-17 16:29:12 +0100

    alloco matrice ---> salvare riferimento in struct

--- so_long/src/map_utility.c
+++ so_long/src/map_utility.c
@@ -3,17 +3,24 @@
 void    alloc_map(t_game *game)
 {
     t_list  *node;
-    char    matrix[game->map.n_row][game->map.n_col];
+    char    matrix[game->map.n_row][game->map.n_col + 1];
     int     i;
-    int     j;
+    int     x;
 
+    x = 0;
     i = 0;
-    j = 0;
     node = game->map.list_map;
-    ft_bzero(matrix, game->map.n_row * game->map.n_col);
-    game->map.map_matrix = matrix;
-    while (j < game->map.n_row && i < game->map.n_col)
+    ft_bzero(matrix, (game->map.n_row * game->map.n_col) + game->map.n_row);
+    game->map.map_matrix = &(*matrix);
+    while (i < game->map.n_row)
     {
-
+        ft_memcpy(&(matrix[i]), node->content, game->map.n_col + 1);
+        node = node->next;
+        i++;
+    }
+    while (x < game->map.n_row)
+    {
+        printf("%s\n", matrix[x]);
+        x++;
     }
 }
