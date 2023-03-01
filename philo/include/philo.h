dc60a512cb6d76bb812376b0d33e32691ed738dc
Sharon Dell unto <sdell-un@e4r5p1.42roma.it>
2023-03-01 23:33:02 +0100

    norme okay!

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -2,28 +2,47 @@
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <stdarg.h>
+# include <string.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdbool.h>
 # include <limits.h>
+# include <thread.h>
+# include <pthread.h>
+# include <synch.h>
+# include <sys/time.h>
+
+typedef struct  s_fork
+{
+    mutex_t *left;
+    mutex_t *right;
+}               t_fork;
 
 typedef struct s_philo
 {
-
+    int         id;
+    int	        expiration;
+    int	        eating_time;
+    int	        sleeping_time;
+    t_fork      fork;
+    int         nbr_meal;
+    //int     status[3];
 }				t_philo;
 
 typedef struct s_table
 {
-	int	nbr_philos;
-	int	expiration;
-	int	eating_time;
-	int	sleeping_time;
-	int	nbr_meals;
+	int      nbr_philos;
+    t_philo     *philo;
+    mutex_t     *fork;
+
 }				t_table;
 
 /*-------------------------check*/
 /*--check.c--*/
-bool	check_args(t_table *table, int ac, char **av);
-bool	is_digit(char *str);
+bool	    check_args(t_table *table, int ac, char **av);
+bool	    is_digit(char *str);
+int         ft_atoi(const char *str);
+void        init_fork(t_table *table);
+void        init_philo(t_table *table);
+uint64_t    get_time(void);
 #endif
--- so_long/src/check_utility.c
+++ so_long/src/check_utility.c
@@ -1,78 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_utility.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdell-un <sdell-un@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 23:29:20 by sdell-un          #+#    #+#             */
+/*   Updated: 2023/03/01 23:30:17 by sdell-un         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./../include/so_long.h"
 
-void    add_char(char *str, char *arr)
+void	add_char(char *str, char *arr)
 {
-    while (*str)
-    {
-       arr[(unsigned char)(*str)]++;
-       ++str;
-    }
+	while (*str)
+	{
+		arr[(unsigned char)(*str)]++;
+		++str;
+	}
 }
 
-void    check_char(t_game *game)
+void	check_char(t_game *game)
 {
-    t_list *node;
-    char arr[256];
-    int i;
+	t_list	*node;
+	char	arr[256];
+	int		i;
 
-    i = 0;
-    ft_bzero(arr, 256);
-    node = game->map.list_map;
-    while (node)
-    {
-        add_char(node->content, arr);
-        node = node->next;
-    }
-    if (!(arr['P'] == 1 && arr['E'] == 1 && arr['C'] >= 1))
-        error("Error\nMust be one P, one E and at least one C\n");
-    while (i < 256)
-    {
-        if (arr[i] != 0 && i != 'P' && i != 'E' && i != 'C' &&
-                i != '\n' && i != '0' && i != '1')
-            error("Error\nFound invalid character!\n");
-        i++;
-    }
-    game->nbr_colls = arr['C'];
+	i = 0;
+	ft_bzero(arr, 256);
+	node = game->map.list_map;
+	while (node)
+	{
+		add_char(node->content, arr);
+		node = node->next;
+	}
+	if (!(arr['P'] == 1 && arr['E'] == 1 && arr['C'] >= 1))
+		error("Error\nMust be one P, one E and at least one C\n");
+	while (i < 256)
+	{
+		if (arr[i] != 0 && i != 'P' && i != 'E' && i != 'C' && \
+				i != '\n' && i != '0' && i != '1')
+			error("Error\nFound invalid character!\n");
+		i++;
+	}
+	game->nbr_colls = arr['C'];
 }
 
-bool side_wall(char *str)
+bool	side_wall(char *str)
 {
-   int i;
+	int	i;
 
-   i = ft_strlen(str);
-   if (*str != '1' && str[i - 1] != '1')
-       return (false);
-   else
-       return (true);
+	i = ft_strlen(str);
+	if (*str != '1' && str[i - 1] != '1')
+		return (false);
+	else
+		return (true);
 }
 
-bool all_one(char *str)
+bool	all_one(char *str)
 {
-    while (*str)
-    {
-        if (*str != '1')
-            return false;
-        ++str;
-    }
-    return (true);
+	while (*str)
+	{
+		if (*str != '1')
+			return (false);
+		++str;
+	}
+	return (true);
 }
 
-bool continuous_wall(t_list *list)
+bool	continuous_wall(t_list *list)
 {
-    t_list *first_row;
+	t_list	*first_row;
 
-    first_row = list;
-    while (list)
-    {
-        if (list == first_row || list->next == NULL)
-        {
-            if (!all_one((char *)list->content))
-                return (false);
-        }
-        else
-            if (!side_wall((char *)list->content))
-                return (false);
-        list = list->next;
-    }
-    return (true);
+	first_row = list;
+	while (list)
+	{
+		if (list == first_row || list->next == NULL)
+		{
+			if (!all_one((char *)list->content))
+				return (false);
+		}
+		else
+			if (!side_wall((char *)list->content))
+				return (false);
+		list = list->next;
+	}
+	return (true);
 }
